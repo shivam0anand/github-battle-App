924b0aec35fde3e4ccecee6ce04b4c0fa1022801
shivam0anand <shivam_anand@outlook.com>
2020-03-21 22:13:52 +0530

    components

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div class="loader">Loading...</div>
+    </div>
+    // <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  );
+}
